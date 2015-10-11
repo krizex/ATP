3bb1fae0ee6f308096d7e3b1b5d4eedc9e6628e5
krizex <krizex@gmail.com>
2015-10-11 21:47:10 +0800

    modify

--- casperjs/qunar.js
+++ casperjs/qunar.js
@@ -1,15 +1,23 @@
 var casper = require('casper').create({
-    verbose: true,
-    logLevel: 'debug'
-})
+    pageSettings:{
+        loadImages:  false
+    }
+});
+
+
+var queryDate = casper.cli.get(0)
 
 var fs = require('fs');
 
-casper.start('http://flight.qunar.com/site/oneway_list.htm?searchDepartureAirport=%E5%8D%97%E4%BA%AC&searchArrivalAirport=%E5%A4%A7%E8%BF%9E&searchDepartureTime=2015-10-12&searchArrivalTime=2015-10-15&nextNDays=0&startSearch=true&fromCode=NKG&toCode=DLC&from=qunarindex&lowestPrice=null');
+var queryUrl = "http://flight.qunar.com/site/oneway_list.htm?searchDepartureAirport=%E5%8D%97%E4%BA%AC&searchArrivalAirport=%E5%A4%A7%E8%BF%9E&searchDepartureTime=" + queryDate + "&startSearch=true&fromCode=NKG&toCode=DLC&from=qunarindex&lowestPrice=null"
+
+casper.echo(queryUrl)
+
+casper.start(queryUrl)
 
 casper.waitWhileSelector('.loading', function() {
-    this.echo('.selector is no more!');
-    this.echo(this.getPageContent())
+    //this.echo(this.getPageContent())
+    fs.write('searchResult.html', this.getPageContent(), 'w')
 });
 
 casper.run();
