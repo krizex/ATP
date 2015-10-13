7467429cece30529034b43bc505b48cbcf6b1714
krizex <krizex@gmail.com>
2015-10-13 22:04:11 +0800

    modify

--- launch/launcher.sh
+++ launch/launcher.sh
@@ -3,8 +3,8 @@
 . ~/.bashrc
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
 
-cd ~/dev/ATP/atp
+cd ~/dev/ATP
 
 #echo "`date +'%Y-%m-%d %H:%M:%S'`" >> tmp.log
 
-python startWork.py ../casperjs/qunar.js
+python atp/startWork.py ../casperjs/qunar.js >> launch/run.log 2>&1
