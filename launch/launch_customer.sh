9fd2868c2dcb8cfc6eb8c79f7e59a7d319ca2e75
krizex <krizex@gmail.com>
2015-10-23 21:17:43 +0800

    redirect error to err.log

--- launch/launch_customer.sh
+++ launch/launch_customer.sh
@@ -3,7 +3,7 @@
 . ~/.bashrc
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
 
-cd ~/dev/ATP
-export PYTHONPATH="`pwd`/atp"
+cd ~/dev/ATP/launch
+export PYTHONPATH="`pwd`/../atp"
 
-python atp/custom/start_custom_work.py casperjs/qunar.js >> launch/custom_run.log 2>&1
+python ../atp/custom/start_custom_work.py ../casperjs/qunar.js >> err.log 2>&1
--- launch/launcher.sh
+++ launch/launcher.sh
@@ -3,8 +3,8 @@
 . ~/.bashrc
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
 
-cd ~/dev/ATP
+cd ~/dev/ATP/launch
 
 #echo "`date +'%Y-%m-%d %H:%M:%S'`" >> tmp.log
 
-python atp/start_work.py >> launch/run.log 2>&1
+python ../atp/start_work.py >> err.log 2>&1
