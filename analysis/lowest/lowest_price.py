0cb157415f02d8e2d533fc7baa51a5f828af2162
David <david.qian@calix.com>
2016-01-28 16:39:25 +0800

    add analysis

--- analysis/lowest/lowest_price.py
+++ analysis/lowest/lowest_price.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from analysis.lowest.result import ResultMap
 from atp.dbc import DB
 
 __author__ = 'David Qian'
@@ -12,17 +13,26 @@ Created on 01/28/2016
 
 
 def analysis_lowest():
+    result_map = ResultMap()
     db = DB('atp', 'atp', 'atp')
     conn = db.getConn()
     cursor = conn.cursor()
     cursor.execute('select * from FLIGHT_LOWEST_PRICE_INFO')
+    print cursor.rowcount
     while True:
         result = cursor.fetchone()
+        print result
         if not result:
             break
 
-        print result
+        query_date = result[0]
+        dep_date = result[4]
+        price = result[10]
+        result_map.add((dep_date - query_date).days, price)
         break
+
+    result_map.analysis()
+
 
 
 if __name__ == '__main__':
---
+++ analysis/lowest/result.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'David Qian'
+
+"""
+Created on 01/28/2016
+@author: David Qian
+
+"""
+
+
+class ResultMap(object):
+    def __init__(self):
+        self.result = {}
+
+    def add(self, date_delta, price):
+        if date_delta in self.result:
+            m = self.result[date_delta]
+            m[0] += price
+            m[1] += 1
+        else:
+            m = [price, 1]
+            self.result[date_delta] = m
+
+    def analysis(self):
+        for i in range(60):
+            if i not in self.result:
+                continue
+            m = self.result[i]
+            print '{}    {}'.format(i, m[0]/m[1])
+
